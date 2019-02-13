c2a855826e034e3242f8a3a51b6b8bb227c123df
Travis Lang <tlang50@icloud.com>
2019-02-13 00:23:01 -0600

    add styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+    height: 100vh;
+    width: 100vw;
+    background-color: #212121;
+    padding-top: 50px;
+`
+
+const Title = styled.h1`
+    color: white;
+`
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <Div>
+        <Title>Movie Sentiment</Title>
+      </Div>
     );
   }
 }
