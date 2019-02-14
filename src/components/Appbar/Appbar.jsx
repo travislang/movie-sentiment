fe73a9c71138723e73bbd992c01fb6bc7e536e4f
Travis Lang <tlang50@icloud.com>
2019-02-13 22:01:35 -0600

    basic nav styling

---
+++ src/components/Appbar/Appbar.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import AppLogo from './AppLogo';
+
+const Div = styled.div`
+    height: 55px;
+    width: 100vw;
+    background-color: #191919;
+    padding: 8px 32px;
+    display: flex;
+    align-items: center;
+`
+const AppLogoContainer = styled.div`
+    height: 45px;
+    display: flex;
+    align-items: center;
+`
+
+const Title = styled.h1`
+    color: white;
+    margin: 0;
+    padding-left: 8px;
+    letter-spacing: 3px;
+`
+const TitleSpan = styled(Title)`
+    font-weight: 200;
+`
+
+class Appbar extends Component {
+    render() {
+        return (
+            <Div>
+                <AppLogoContainer>
+                    {AppLogo}
+                    <Title><TitleSpan as="span">Movie</TitleSpan>Ai</Title>
+                </AppLogoContainer>
+            </Div>
+        );
+    }
+}
+
+export default Appbar;
