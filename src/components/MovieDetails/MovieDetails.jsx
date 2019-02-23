45f1a22f503645052597014f38758a3811c785a8
Travis Lang <tlang50@icloud.com>
2019-02-23 10:58:57 -0600

    add details header

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -13,18 +13,41 @@ const Div = styled.div`
 
 const ImgDiv = styled.div`
     width: 100vw;
-    min-height: 500px;
-    background-color: blue;
+    min-height: 30vmax;
+    margin-top: 71px;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+
+    &:before {
+        content: '';
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        background: inherit;
+        filter: grayscale(50%);
+    }
 `
 const Img = styled.img`
     width: 100%;
     height: 100%;
     object-fit: cover;
 `
-const Title = styled.h3`
+const Title = styled.h1`
     margin: 0;
     padding: 5px;
-    color: grey;
+    font-family: 'Roboto';
+    font-size: 7em;
+    
+    font-weight: bold;
+    color: #d2202f;
+    z-index: 100;
+    text-align: center;
+    opacity: 0.75;
 `
 
 const MovieDetails = (props) => {
@@ -50,13 +73,9 @@ const MovieDetails = (props) => {
 
                 return (
                     <Div>
-                        <ImgDiv>
-                            <Img src={`http://image.tmdb.org/t/p/w1280/${movie.backdropPath}`} />
+                        <ImgDiv style={{ background: `center/cover url(http://image.tmdb.org/t/p/w1280/${movie.backdropPath})`}}>
+                            <Title>{movie.name}</Title>
                         </ImgDiv>
-
-                        <Title>
-                            {movie.name}
-                        </Title>
                     </Div>
                 )
             }}
