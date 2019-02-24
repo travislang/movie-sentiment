85c96c4a6c98976d6603192b65ad956591a3af38
Travis Lang <tlang50@icloud.com>
2019-02-24 17:59:40 -0600

    custom infinite scroll working v1

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -26,30 +26,93 @@ const handleClick = (props, id) => {
     props.history.push(`/details/${id}`)
 }
 
+
+const QUERY = gql`
+        query getPopularMovies($page: Int) {
+            getPopularMovies(page: $page) {
+                id
+                name
+                posterPath
+            }
+        }
+    `
+class MovieFeed extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {page: 1}
+        console.log('create', props.onLoadMore);
+            
+            window.onscroll = () => {
+                console.log('document.documentElement.offsetHeight', document.documentElement.offsetHeight);
+                
+                if (
+                    window.innerHeight + document.documentElement.scrollTop
+                    >= document.documentElement.offsetHeight - 100
+                ) {
+                    this.setState({
+                        page: this.state.page + 1
+                    })
+                    props.onLoadMore(this.state.page);
+                }
+            };
+        }
+
+
+    render() {
+        const { movies = [], onLoadMore, ...props } = this.props;
+        return (
+            <>
+                {movies.map(({ name, id, posterPath }) => (
+                    <Div key={id}>
+                        <Img onClick={() => handleClick(props, id)} src={`http://image.tmdb.org/t/p/w342/${posterPath}`} />
+                        <Title>
+                            {name}
+                        </Title>
+                    </Div>
+                ))}
+            </>
+        )
+    }
+}
+
+
 const MovieCard = (props) => (
     <Query
-        query={gql`
-      {
-        getPopularMovies {
-            id
-            name
-            posterPath
-        }
-      }
-    `}
+        query={QUERY}
+        variables={{
+            page: 1
+        }}
     >
-        {({ loading, error, data }) => {
+        {({ loading, error, data, fetchMore, variables }) => {
+            
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error :(</p>;
-
-            return data.getPopularMovies.map(({ name, id, posterPath }) => (
-                <Div key={id}>
-                    <Img onClick={() => handleClick(props, id)} src={`http://image.tmdb.org/t/p/w342/${posterPath}`} />
-                    <Title>
-                        {name}
-                    </Title>
-                </Div>
-            ));
+                console.log('d', variables);
+                
+            return (
+                <MovieFeed 
+                    movies={data.getPopularMovies || []} 
+                    onLoadMore={(page) => {
+                        console.log('inloadmore', page);
+                        
+                        return (fetchMore({
+                            variables: {
+                                page: page,
+                            },
+                            updateQuery: (prev, { fetchMoreResult }) => {
+                                if (!fetchMoreResult) return prev;
+                                return Object.assign({}, prev, {
+                                    getPopularMovies: [...prev.getPopularMovies, ...fetchMoreResult.getPopularMovies],
+                                });
+                            },
+                        }))
+                    }
+                        
+                    }
+                />
+            )
+            
+            
         }}
     </Query>
 );
