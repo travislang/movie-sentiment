4e3f46206f5088959af849e93fd031f70cacff38
Travis Lang <tlang50@icloud.com>
2019-02-27 14:41:01 -0600

    change postgres uri for heroku

--- server/modules/orm.config.js
+++ server/modules/orm.config.js
@@ -1,6 +1,6 @@
 const Sequalize = require('sequelize');
 
-const connectionString = 'postgres://localhost:5432/movie_sentiment'
+const connectionString = 'postgres://oigvqgvvrhzazm:8b194a2aaeed8076cf8cdd4af4db96afbb12bce3666bb2adf9a29a760f6d9b3e@ec2-174-129-236-21.compute-1.amazonaws.com:5432/dfrloljl3kr92n'
 
 const sequelize = new Sequalize(connectionString, {
     operatorsAliases: false
